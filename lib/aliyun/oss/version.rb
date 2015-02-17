939f1365e1a9c776a336b1ebc73119a125300d1b
Teddy <theodore.widom@gmail.com>
2015-02-16 19:30:32 -0500

    Remove beta version number

--- lib/aliyun/oss/version.rb
+++ lib/aliyun/oss/version.rb
@@ -5,10 +5,8 @@ module Aliyun
       MAJOR    = '0'
       MINOR    = '7'
       TINY     = '0'
-      #BETA     = nil # Time.now.to_i.to_s
-      BETA     = Time.now.to_i.to_s
     end
-    
-    Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
+
+    Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY].compact * '.'
   end
 end
